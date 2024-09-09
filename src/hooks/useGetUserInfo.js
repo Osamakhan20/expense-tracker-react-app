0ae5a017d6cc307f2319d136fa3e9f37cf3f72c8
Osamakhan20 <osmiikhan137@gmail.com>
2024-09-09 20:00:56 +0500

    fixed

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,32 +1,7 @@
-/*export const useGetUserInfo = () => {
-  const { name, profilePhoto, isAuth, userID } = JSON.parse(
-    localStorage.getItem("auth")
-  );
-    return { name, profilePhoto, isAuth, userID };
-}; */
 
 export const useGetUserInfo = () => {
-    let userInfo = {
-      name: '',
-      profilePhoto: '',
-      isAuth: false,
-      userID: null,
-    };
-  
-    try {
-      const authData = localStorage.getItem("auth");
-      if (authData) {
-        const parsedData = JSON.parse(authData);
-        userInfo = {
-          name: parsedData.name || '',
-          profilePhoto: parsedData.profilePhoto || '',
-          isAuth: parsedData.isAuth || false,
-          userID: parsedData.userID || null,
-        };
-      }
-    } catch (error) {
-      console.error("Failed to parse auth data from localStorage", error);
-    }
-  
-    return userInfo;
-  };
+  const { name, profilePhoto, userID, isAuth } =
+    JSON.parse(localStorage.getItem("auth")) || {};
+
+  return { name, profilePhoto, userID, isAuth };
+};
